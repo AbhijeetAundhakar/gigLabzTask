e9219911b85e3f4955d73b709a3b1fcd7856f1c4
Abhijeet <abhiaundhakar@gmail.com>
2025-03-26 12:33:20 +0530

    Removed unnecessary code.

--- EmployeeManagement/src/main/java/com/employeemanagement/EmployeeManagement/service/MyUserDetailsService.java
+++ EmployeeManagement/src/main/java/com/employeemanagement/EmployeeManagement/service/MyUserDetailsService.java
@@ -1,29 +1,4 @@
 package com.employeemanagement.EmployeeManagement.service;
-//
-//import com.employeemanagement.EmployeeManagement.model.User;
-//import com.employeemanagement.EmployeeManagement.repository.UserRepo;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//public class MyUserDetailsService implements UserDetailsService {
-//
-//    @Autowired
-//    private UserRepo repo;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-//        User user = repo.findByUsername(username);
-//        if (user == null) {
-//            throw new UsernameNotFoundException("User not found");
-//        }
-//        return new UserPrincipal(user);
-//    }
-//}
-
 
 import com.employeemanagement.EmployeeManagement.model.User;
 import com.employeemanagement.EmployeeManagement.repository.UserRepo;
